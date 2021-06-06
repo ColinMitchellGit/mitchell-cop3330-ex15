b76cca5fd563f4c84d737bd19f541ae4f75e9394
Colin Mitchell <benhawk6003@gmail.com>
2021-06-06 15:58:03 -0400

    exercise 15

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Colin Mitchell
+ */
+
+package org.example;
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the password? ");
+        String password = input.nextLine();
+
+        if(password.equals("abc$123")) {
+            System.out.println("Welcome!");
+        }else {
+            System.out.println("I don't know you.");
+        }
+    }
+}
